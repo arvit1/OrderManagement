aad6e7bf7e97f20658388f22a05d929b1500439e
arvit <avarfaj86@gmail.com>
2021-01-29 12:12:53 +0100

    query native

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,17 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {EntityManager} from 'typeorm';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly entityManager: EntityManager
+  ) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  async getHello() {
+    return await this.entityManager.query("SELECT * FROM user");
+    // return this.appService.getHello();
   }
 }
