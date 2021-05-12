07534f4a28e6b2ce7f237d3d4da15a6bfd60f124
Enxhi Dërvishi <enxh.dervishi@gmail.com>
2021-05-12 11:31:11 +0200

    transaction

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -31,66 +31,84 @@ async function bootstrap() {
 }
 
 async function initDB() {
-
-  await  getConnection().transaction(async transactionalEntityManager => {
+  let adminRole;
+  let dyqanRole;
+  let magazinaRole;
+  await getConnection().transaction(async transactionalEntityManager => {
 
     // Crea Ruolo ADMIN
-    const adminRole = await transactionalEntityManager.connection.getRepository(Role).findOne(1).then(async (role) => {
+    adminRole = await transactionalEntityManager.connection.getRepository(Role).findOne(1).then(async (role) => {
       // tslint:disable-next-line:triple-equals
       if (role === undefined) {
-        role = await transactionalEntityManager.connection.getRepository(Role).save({ id: 1, name: 'ADMIN'});
+        role = await transactionalEntityManager.connection.getRepository(Role).save({id: 1, name: 'ADMIN'});
       }
       return role;
     });
+  });
 
+  await getConnection().transaction(async transactionalEntityManager => {
     // Crea Ruolo MEMBER
-    const magazinaRole = await transactionalEntityManager.connection.getRepository(Role).findOne(2).then(async (role) => {
+    magazinaRole = await transactionalEntityManager.connection.getRepository(Role).findOne(2).then(async (role) => {
       if (role === undefined) {
-        role = await transactionalEntityManager.connection.getRepository(Role).save({ id: 2, name: 'MAGAZINA'});
+        role = await transactionalEntityManager.connection.getRepository(Role).save({id: 2, name: 'MAGAZINA'});
       }
       return role;
     });
+  });
 
-    const dyqanRole = await transactionalEntityManager.connection.getRepository(Role).findOne(3).then(async (role) => {
+  await getConnection().transaction(async transactionalEntityManager => {
+    dyqanRole = await transactionalEntityManager.connection.getRepository(Role).findOne(3).then(async (role) => {
       if (role === undefined) {
-        role = await transactionalEntityManager.connection.getRepository(Role).save({ id: 3, name: 'DYQAN'});
+        role = await transactionalEntityManager.connection.getRepository(Role).save({id: 3, name: 'DYQAN'});
       }
       return role;
     });
+  });
 
-    const saltRounds = 10;
-    // Create ADMIN
+  const saltRounds = 10;
+  // Create ADMIN
+  await getConnection().transaction(async transactionalEntityManager => {
     await transactionalEntityManager.connection.getRepository(User).findOne(1).then(async (user) => {
       if (user === undefined) {
         bcrypt.genSalt(saltRounds, (err, salt) => {
           bcrypt.hash('password', salt, async (err2, hash) => {
             await transactionalEntityManager.connection.getRepository(User).save(
-              { id: 1, username: 'admin', email: 'admin@admin.it', password: hash, roles: [adminRole, magazinaRole, dyqanRole],
-                firstName: 'adminFirst', lastName: 'adminLast'});
+              {
+                id: 1, username: 'admin', email: 'admin@admin.it', password: hash, roles: [adminRole, magazinaRole, dyqanRole],
+                firstName: 'adminFirst', lastName: 'adminLast',
+              });
           });
-       });
+        });
       }
     });
+  });
 
+  await  getConnection().transaction(async transactionalEntityManager => {
     await transactionalEntityManager.connection.getRepository(User).findOne(2).then(async (user) => {
       if (user === undefined) {
         bcrypt.genSalt(saltRounds, (err, salt) => {
           bcrypt.hash('password', salt, async (err2, hash) => {
             await transactionalEntityManager.connection.getRepository(User).save(
-              { id: 2, username: 'magazina', email: 'magazina@member.it', password: hash, roles: [magazinaRole, dyqanRole],
-                firstName: 'magazinaFirst', lastName: 'magazinaLast'});
+              {
+                id: 2, username: 'magazina', email: 'magazina@member.it', password: hash, roles: [magazinaRole, dyqanRole],
+                firstName: 'magazinaFirst', lastName: 'magazinaLast',
+              });
           });
-       });
+        });
       }
     });
+  });
 
+  await  getConnection().transaction(async transactionalEntityManager => {
     await transactionalEntityManager.connection.getRepository(User).findOne(3).then(async (user) => {
       if (user === undefined) {
         bcrypt.genSalt(saltRounds, (err, salt) => {
           bcrypt.hash('password', salt, async (err2, hash) => {
             await transactionalEntityManager.connection.getRepository(User).save(
-              { id: 3, username: 'dyqan', email: 'dyqan@member.it', password: hash, roles: [dyqanRole],
-                firstName: 'dyqanFirst', lastName: 'dyqanLast'});
+              {
+                id: 3, username: 'dyqan', email: 'dyqan@member.it', password: hash, roles: [dyqanRole],
+                firstName: 'dyqanFirst', lastName: 'dyqanLast',
+              });
           });
         });
       }
